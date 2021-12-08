3dd4c3b01eb397e71033f1254f22f5f1a202fd1a
Kartik Andalam <kartik.andalam@altonomy.com>
2021-12-08 17:16:17 +0800

    updated port number to 8080

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const svc = app.get<CharactersService>(CharactersService)
   await svc.init()
-  await app.listen(3000)
+  await app.listen(8080)
 }
 bootstrap()
